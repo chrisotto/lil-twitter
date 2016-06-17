0aa763679ebfcb262070ed74ae4fb88e6dab7d5b
David <email": null>
2016-06-17 13:45:48 -0700

    eddit migration

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,5 @@
 class Friend < ActiveRecord::Base
   # Remember to create a migration!
-  belongs_to :follower, class_name: 'User'
-  belongs_to :followee, class_name: 'User'
+  belongs_to :follower, class_name: 'User', foreign_key: :follower_id
+  belongs_to :followee, class_name: 'User', foreign_key: :followee_id
 end
