8d0332c1efeb7dcd9e7023581004222d011040bd
Chris Otto <cotto@yahoo.com>
2016-06-16 18:55:58 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :email, :password
   validates :email, uniqueness: true
   has_many :tweets
+  
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
