d165f5e755bde90cf793734d969c27b21e83bfbb
Jason Sutter <jasonsutter87@gmail.com>
2016-06-17 14:34:34 -0700

    typeo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,8 @@
 require 'faker'
 
-
 User.delete_all
 Friend.delete_all
 Tweet.delete_all
-
-# 20.times do
-
-#   u1 = User.new(name: Faker::Name.name , email: , password: )
-#   u2 = Tweet.new(name: , email: , password: )
-#   u1.save
-#   u2.save
-#   Friend.create(follower_id: u1.id, followee_id: u2.id)
-# end
 
 100.times do
   User.create(name: Faker::Name.name,
