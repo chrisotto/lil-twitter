415a65a8a14a829bb90dfb0df0d11d424cd8a0e3
David <email": null>
2016-06-16 20:44:18 -0700

    add a registration form and add routes also update controler

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,4 +1,20 @@
+get '/users/new' do
+  erb :"/user/new"
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    erb :'users/new'
+  end
+end
+
 get '/users/:id' do
   @user =User.find(params[:id])
   erb :"user/show"
 end
+
+
