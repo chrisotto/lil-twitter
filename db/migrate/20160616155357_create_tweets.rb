7915fc2a6cbb0b85b85c216d019e0cc9321a9fed
Chris Otto <cotto@yahoo.com>
2016-06-16 18:53:24 -0700

    Update 20160616155357_create_tweets.rb

--- db/migrate/20160616155357_create_tweets.rb
+++ db/migrate/20160616155357_create_tweets.rb
@@ -2,6 +2,7 @@ class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
       t.string :body
+      t.integer :user_id, {null: false}
 
       t.timestamps
     end
