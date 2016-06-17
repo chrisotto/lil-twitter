1f6f67da00e512bd45110c3a6cbd1aba15580d41
David <davidsramirez11@gmail.com>
2016-06-17 11:20:30 -0700

    Clean up formatting

--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -3,9 +3,7 @@ get '/tweet/new' do
 end
 
 post '/tweet' do
-p params
-p "3"*24
-@user =User.find(session[:id])
+  @user = User.find(session[:id])
   @tweet = Tweet.new( body: params[:tweet][:body], user_id: session[:id])
   if @tweet.save
     redirect "/users/#{@user.id}"
